1ee041a0d07c9caf3886076e6caa86ddda20a16d
Lucas <72669100+CiceroLucas@users.noreply.github.com>
2026-06-16 09:04:51 -0300

    Update gerador.php

--- gerador.php
+++ gerador.php
@@ -5,4 +5,4 @@ for ($i = 1; $i <= 1000; $i++) {
     fputcsv($handle, [$i, "status_disponivel_{$i}", rand(10, 500)]);
 }
 fclose($handle);
-echo "Arquivo de teste com 100.00 linhas gerado com sucesso!\n";
+echo "Arquivo de teste com 1000 linhas gerado com sucesso!\n";
